9040e3069baaf0d5b0459a3305d7744109558887
Talya <tchavice@gmail.com>
2020-06-15 12:58:12 -0400

    Initial repo

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/talya/Documents/C++_Course/Cpp_Final_Project/src/controller.cpp" "/home/talya/Documents/C++_Course/Cpp_Final_Project/build/CMakeFiles/SnakeGame.dir/src/controller.cpp.o"
+  "/home/talya/Documents/C++_Course/Cpp_Final_Project/src/game.cpp" "/home/talya/Documents/C++_Course/Cpp_Final_Project/build/CMakeFiles/SnakeGame.dir/src/game.cpp.o"
+  "/home/talya/Documents/C++_Course/Cpp_Final_Project/src/main.cpp" "/home/talya/Documents/C++_Course/Cpp_Final_Project/build/CMakeFiles/SnakeGame.dir/src/main.cpp.o"
+  "/home/talya/Documents/C++_Course/Cpp_Final_Project/src/renderer.cpp" "/home/talya/Documents/C++_Course/Cpp_Final_Project/build/CMakeFiles/SnakeGame.dir/src/renderer.cpp.o"
+  "/home/talya/Documents/C++_Course/Cpp_Final_Project/src/snake.cpp" "/home/talya/Documents/C++_Course/Cpp_Final_Project/build/CMakeFiles/SnakeGame.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
